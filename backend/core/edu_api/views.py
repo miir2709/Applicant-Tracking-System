6a38d14c3f60c7ebfb4c2f2550b276a887992617
Ay9040 <ayush.khade04@outlook.com>
2022-03-10 21:55:16 +0530

    Added Education Details Create API

---
+++ backend/core/edu_api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from core.edu.models import EducationDetails
+from .serializers import EducationSerializer
+
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = EducationDetails.education_objects.all()
+    serializer_class = EducationSerializer
+    http_method_names = ["get", "post"]
--- backend/core/routers.py
+++ backend/core/routers.py
@@ -1,18 +1,17 @@
 from rest_framework.routers import SimpleRouter
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet
-
+from .edu_api.views import EducationViewSet
 
 routes = SimpleRouter()
 
 # AUTHENTICATION
-routes.register(r'auth/login', LoginViewSet, basename='auth-login')
-routes.register(r'auth/register', RegistrationViewSet, basename='auth-register')
-routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+routes.register(r"auth/login", LoginViewSet, basename="auth-login")
+routes.register(r"auth/register", RegistrationViewSet, basename="auth-register")
+routes.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
 
 # USER
-routes.register(r'user', UserViewSet, basename='user')
+routes.register(r"user", UserViewSet, basename="user")
 
-urlpatterns = [
-    *routes.urls
-]
+routes.register(r"edu", EducationViewSet, basename="edu")
+urlpatterns = [*routes.urls]
