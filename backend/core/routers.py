2b42909fae4d2a974410503476e3e8423d5b0550
Ay9040 <ayush.khade04@outlook.com>
2022-03-15 17:02:58 +0530

    Added recruiter details

--- backend/core/routers.py
+++ backend/core/routers.py
@@ -3,6 +3,7 @@ from core.user.viewsets import UserViewSet
 from core.auth.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet
 from .edu_api.views import EducationViewSet
 from .employment_details_api.views import EmploymentDetailsViewSet
+from .recruiter_api.views import RecruiterViewSet
 
 
 routes = SimpleRouter()
@@ -16,6 +17,7 @@ routes.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
 routes.register(r"user", UserViewSet, basename="user")
 
 routes.register(r"edu", EducationViewSet, basename="edu")
+routes.register(r"recruiter", RecruiterViewSet, basename="recruiter")
 routes.register(r"employment_details", EmploymentDetailsViewSet, basename="employment_details")
 
 urlpatterns = [*routes.urls]
