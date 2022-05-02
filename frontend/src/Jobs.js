37b58cf5592c3780767d0a280b0d40c545dd3bf1
Ayush <ayush.khade04@outlook.com>
2022-05-02 18:31:43 +0530

    added filters

--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -4,6 +4,15 @@ import axios from 'axios'
 
 function Jobs(props) {
     const [jobPosts, setJobPosts] = useState([])
+    const [filters, setFilters] = useState({"category": null, "location": null})
+    var job_c = {
+        "EN": "engineering",
+        "SA": "sales",
+        "BU": "business",
+        "ET": "entertainment",
+        "F": "food",
+        "O": "other"
+    }
     useEffect(async function () {
         let resp;
         await axios.get('http://127.0.0.1:8000/api/job_posts/').then((data) => {
@@ -11,17 +20,33 @@ function Jobs(props) {
         })
         console.log(resp)
         setJobPosts(resp)
+        setFilters({"category": job_c[resp[0].job_category], "location": resp[0].location})
     }, [])
-
     function showJob(id) {
         window.location.href = "/job_post/" + id
     }
 
     return (
         <div style={{ paddingTop: "80px" }}>
+            <div className="max-width bg-gray-300 pt-4 pb-2 px-3 flex flex-row mx-4 mt-3 rounded-xl">
+                <select name="category" className="mx-5" onChange={(e) => setFilters({"category": e.target.value, "location": filters.location})}>
+                    <option value="engineering">Engineering</option>
+                    <option value="sales">Sales</option>
+                    <option value="business">Business</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="other">Other</option>
+                </select>
+                <select className="mx-5" onChange={(e) => setFilters({"category": filters.category, "location": e.target.value})}>{
+                    jobPosts.map((data, index) => {
+                        return <option value={data['location'].toLowerCase()}>{data['location']}</option>
+                    })
+                }
+                </select>
+            </div>
             {jobPosts.map((data, index) => {
                 let skills = data['skills_required'].split(',')
-                console.log(skills)
+                console.log(filters, data)
+                if((filters.category == job_c[data['job_category']] && filters.location == data['location']))
                 return (
                     <div className="jobpost flex flex-col text-left rounded-lg border-solid border-2 border-gray-400 m-12 p-10 hover:shadow-lg cursor-pointer" onClick={() => showJob(data['id'])}>
                         <div className="text-3xl hover:text-gray-300">{data['job_title']}</div>
