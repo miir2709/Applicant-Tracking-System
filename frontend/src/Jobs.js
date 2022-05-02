12c298082c78260b290aa8bdd0480e24c1519b7b
Ayush <ayush.khade04@outlook.com>
2022-05-02 20:17:51 +0530

    Fixed filter bug

--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 function Jobs(props) {
     const [jobPosts, setJobPosts] = useState([])
     const [filters, setFilters] = useState({"category": null, "location": null})
+    const [locations, setLocations] = useState([]);
     var job_c = {
         "EN": "engineering",
         "SA": "sales",
@@ -18,9 +19,15 @@ function Jobs(props) {
         await axios.get('http://127.0.0.1:8000/api/job_posts/').then((data) => {
             resp = (data['data'])
         })
+        var locationList = []
         console.log(resp)
         setJobPosts(resp)
-        setFilters({"category": job_c[resp[0].job_category], "location": resp[0].location})
+        for(var i = 0; i < resp.length; i++) {
+            locationList.push(resp[i]['location'])
+        }
+        locationList = [...new Set(locationList)]
+        setLocations(locationList)
+        setFilters({"category": job_c[resp[0].job_category], "location": resp[0].location.toLowerCase()})
     }, [])
     function showJob(id) {
         window.location.href = "/job_post/" + id
@@ -36,9 +43,9 @@ function Jobs(props) {
                     <option value="entertainment">Entertainment</option>
                     <option value="other">Other</option>
                 </select>
-                <select className="mx-5" onChange={(e) => setFilters({"category": filters.category, "location": e.target.value})}>{
-                    jobPosts.map((data, index) => {
-                        return <option value={data['location'].toLowerCase()}>{data['location']}</option>
+                <select className="mx-5" onChange={(e) => setFilters({"category": filters.category, "location": e.target.value.toLowerCase()})}>{
+                    locations.map((data, index) => {
+                        return <option value={data.toLowerCase()}>{data}</option>
                     })
                 }
                 </select>
@@ -46,17 +53,17 @@ function Jobs(props) {
             {jobPosts.map((data, index) => {
                 let skills = data['skills_required'].split(',')
                 console.log(filters, data)
-                if((filters.category == job_c[data['job_category']] && filters.location == data['location']))
-                return (
-                    <div className="jobpost flex flex-col text-left rounded-lg border-solid border-2 border-gray-400 m-12 p-10 hover:shadow-lg cursor-pointer" onClick={() => showJob(data['id'])}>
-                        <div className="text-3xl hover:text-gray-300">{data['job_title']}</div>
-                        <div className="flex flex-row mt-5">
-                            <div>{data['recruiter_id']['company_name']}</div>
-                            <div className="ml-10 flex flex-row"><i className='fa-solid fa-location-dot mr-1 mt-1'></i><p>{data['location']}</p></div>
-                            <div className="justify-self-end m-auto mr-0">{'Deadline: ' + data['application_deadline']}</div>
+                if((filters.category == job_c[data['job_category']] && filters.location == data['location'].toLowerCase()))
+                    return (
+                        <div className="jobpost flex flex-col text-left rounded-lg border-solid border-2 border-gray-400 m-12 p-10 hover:shadow-lg cursor-pointer" onClick={() => showJob(data['id'])}>
+                            <div className="text-3xl hover:text-gray-300">{data['job_title']}</div>
+                            <div className="flex flex-row mt-5">
+                                <div>{data['recruiter_id']['company_name']}</div>
+                                <div className="ml-10 flex flex-row"><i className='fa-solid fa-location-dot mr-1 mt-1'></i><p>{data['location']}</p></div>
+                                <div className="justify-self-end m-auto mr-0">{'Deadline: ' + data['application_deadline']}</div>
+                            </div>
                         </div>
-                    </div>
-                )
+                    )
             })}
         </div >
     )
